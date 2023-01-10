67218c76f527803cb9fd63c5806c9789514592be
Sansar Maske <sansarmaske@gmail.com>
2023-01-10 17:21:50 +0545

    1.2: course information, step2

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,8 +1,12 @@
+import Part from "./Part"
 const Content= (props) => {
     return(
-        <p>
-        {props.part} {props.exercises}
-      </p>
+        <div>
+            <Part part={props.part1} exercises={props.exercises1}/>
+            <Part part={props.part2} exercises={props.exercises2}/>
+            <Part part={props.part3} exercises={props.exercises3}/>
+        
+      </div>
     )
 }
 
